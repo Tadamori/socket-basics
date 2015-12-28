13eebea07afd926e1decfc07b9f17b85cde21027
Artem Lyubutov <lubytov@me.com>
2015-12-28 19:35:01 +0500

    Add name support

--- public/script/app.js
+++ public/script/app.js
@@ -1,4 +1,8 @@
 var socket = io();
+var name = getQueryVariable('name') || 'User';
+var room = getQueryVariable('room');
+
+console.log(name + ' wants to join ' + room);
 
 socket.on('connect', function () {
 	console.log('Connected to socket.io server!');
@@ -6,11 +10,12 @@ socket.on('connect', function () {
 
 socket.on('message', function (message) {
 	var momentTimestamp = moment.utc(message.timestamp);
+	var $message = $('.messages');
 	console.log('New message:');
 	console.log(message.text);
 
-	
-	$('.messages').append('<p><strong>' + momentTimestamp.local().format('h:mm a') + '</strong>: ' + message.text + '</p>');
+	$message.append('<p><strong>' + message.name + ' ' + momentTimestamp.local().format('h:mm a') + '</strong></p> ');
+	$message.append('<p>' + message.text + '</p>');
 });
 
 var $form = $('#message-form');
@@ -20,6 +25,7 @@ $form.on('submit', function (event) {
 	$message = $form.find('input[name=message]');
 
 	socket.emit('message', {
+		name: name,
 		text: $message.val()
 	});
 

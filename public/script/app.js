1307784af80c0d80acb740199d7fe173dd1cd6e6
Artem Lyubutov <lubytov@me.com>
2015-12-28 18:01:25 +0500

    Add timestamp

--- public/script/app.js
+++ public/script/app.js
@@ -5,11 +5,12 @@ socket.on('connect', function () {
 });
 
 socket.on('message', function (message) {
+	var momentTimestamp = moment.utc(message.timestamp);
 	console.log('New message:');
 	console.log(message.text);
 
 	
-	$('.messages').append('<p>' + message.text + '</p>');
+	$('.messages').append('<p><strong>' + momentTimestamp.local().format('h:mm a') + '</strong>: ' + message.text + '</p>');
 });
 
 var $form = $('#message-form');

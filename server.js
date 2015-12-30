6445381b5c7d27c2d2cc89ce5a9cbd312d336359
Artem Lyubutov <lubytov@me.com>
2015-12-30 18:18:14 +0500

    Add styles

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 var PORT = process.env.PORT || 3000;
+var moment = require('moment');
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var moment = require('moment');
 
 app.use(express.static(__dirname + '/public'));
 
 var clientInfo = {};
 
-function sendCurrentUSers(socket) {
+// Sends current users to provided socket
+function sendCurrentUsers (socket) {
 	var info = clientInfo[socket.id];
 	var users = [];
 
@@ -26,52 +27,55 @@ function sendCurrentUSers(socket) {
 	});
 
 	socket.emit('message', {
-		name: 'System',
-		text: 'Current users: ' + users.join(', '),
+		name: 'Дух чата',
+		text: 'Сейчас в чате: ' + users.join(', '),
 		timestamp: moment().valueOf()
-	})
+	});
 }
 
 io.on('connection', function (socket) {
-	console.log('user connected via socket.io!');
+	console.log('User connected via socket.io!');
 
 	socket.on('disconnect', function () {
 		var userData = clientInfo[socket.id];
+
 		if (typeof userData !== 'undefined') {
 			socket.leave(userData.room);
 			io.to(userData.room).emit('message', {
-				name: 'System',
-				text: userData.name + ' has left',
+				name: 'Дух чата',
+				text: userData.name + ' покинул(а) чат!',
 				timestamp: moment().valueOf()
 			});
 			delete clientInfo[socket.id];
 		}
 	});
 
-		socket.on('joinRoom', function(req) {
-			clientInfo[socket.id] = req;
-			socket.join(req.room);
-			socket.broadcast.to(req.room).emit('message', {
-				name: 'System',
-				text: req.name + ' has joined!',
-				timestamp: moment().valueOf
-			})
+	socket.on('joinRoom', function (req) {
+		clientInfo[socket.id] = req;
+		socket.join(req.room);
+		socket.broadcast.to(req.room).emit('message', {
+			name: 'System',
+			text: req.name + ' присоединил(ась)ся к чату!',
+			timestamp: moment().valueOf()
 		});
+	});
 
-	socket.on('message', function(message) {
-		console.log('Message recieved: ' + message.text);
+	socket.on('message', function (message) {
+		console.log('Message received: ' + message.text);
 
-		if (message.text === '@currentUser') {
-			sendCurrentUSers(socket);
+		if (message.text === '@ктотут') {
+			sendCurrentUsers(socket);
 		} else {
-			message.timestamp = moment().valueOf(); // возвращает unix-timestamp
-			io.to(clientInfo[socket.id].room).emit('message', message);
+			message.timestamp = moment().valueOf();
+			io.to(clientInfo[socket.id].room).emit('message', message);	
 		}
 	});
 
+	// timestamp property - JavaScript timestamp (milliseconds)
+
 	socket.emit('message', {
-		name: 'System',
-		text: 'Welcome to the chat application!',
+		name: 'Дух чата',
+		text: 'Добро пожаловать в чатик!',
 		timestamp: moment().valueOf()
 	});
 });
